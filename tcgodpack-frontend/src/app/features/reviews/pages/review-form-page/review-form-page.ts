a2046d9cec7e139d88c648bcf4155f742e721016
Eskotifai <samd.scottv@gmail.com>
2026-05-26 16:44:03 -0400

    Arreglada la locura del frontend

--- tcgodpack-frontend/src/app/features/reviews/pages/review-form-page/review-form-page.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-review-form-page',
-  imports: [],
-  templateUrl: './review-form-page.html',
-  styleUrl: './review-form-page.css',
-})
-export class ReviewFormPage {}
